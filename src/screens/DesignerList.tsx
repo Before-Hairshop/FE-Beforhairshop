2686613981cf67288b4c6f73fbfebc1458c136ed
UNayoung <skdud@ajou.ac.kr>
2022-08-16 10:15:55 +0900

    impl. designer list page - design v2.

--- src/screens/DesignerList.tsx
+++ src/screens/DesignerList.tsx
@@ -17,6 +17,7 @@ import Header from "../components/Header";
 import GoBackIcon from "../assets/icons/goBack.svg";
 import GoHomeIcon from "../assets/icons/goHome.svg";
 import SearchIcon from "../assets/icons/search.svg";
+import StarIcon from "../assets/icons/star.svg";
 
 import DefaultDesignerImg from "../assets/images/default_designer.png";
 
@@ -27,20 +28,51 @@ const HeaderContents = () => (
   </>
 );
 
+const TagItem = (props: { value: string }) => (
+  <View
+    style={{
+      borderRadius: 100,
+      backgroundColor: "rgba(252, 42, 91, 0)",
+      borderStyle: "solid",
+      borderWidth: 1,
+      borderColor: "#fc2a5b",
+      justifyContent: "center",
+      alignItems: "center",
+      marginRight: verticalScale(5.4),
+      paddingTop: verticalScale(3),
+      paddingBottom: verticalScale(3),
+      paddingLeft: scale(8),
+      paddingRight: scale(8),
+    }}>
+    <Text
+      style={{
+        fontFamily: "Pretendard",
+        fontSize: 10,
+        fontWeight: "500",
+        fontStyle: "normal",
+        letterSpacing: -0.5,
+        textAlign: "left",
+        color: "#fc2a5b",
+      }}>
+      #{props.value}
+    </Text>
+  </View>
+);
+
 const DesignerItem = () => (
   <View
     style={{
       width: "100%",
-      paddingTop: verticalScale(29),
+      height: verticalScale(141),
       paddingRight: scale(20),
-      paddingBottom: verticalScale(28),
       paddingLeft: scale(22),
       flexDirection: "row",
+      alignItems: "center",
     }}>
     <Image
       source={DefaultDesignerImg}
       style={{
-        width: scale(70),
+        width: "21%",
         height: verticalScale(70),
         borderRadius: 70,
         borderWidth: 1,
@@ -48,29 +80,37 @@ const DesignerItem = () => (
       }}
       resizeMode={"cover"}
     />
-    <View>
-      <Text
-        style={{
-          fontFamily: "Pretendard",
-          fontStyle: "normal",
-          fontWeight: "700",
-          fontSize: 15,
-          textAlign: "left",
-          color: "#FFFFFF",
-        }}>
-        이안
-      </Text>
-      <Text
-        style={{
-          fontFamily: "Pretendard",
-          fontStyle: "normal",
-          fontWeight: "600",
-          fontSize: 15,
-          textAlign: "left",
-          color: "#737373",
-        }}>
-        헤어샵 이름
-      </Text>
+    <View
+      style={{
+        width: "74%",
+        paddingLeft: scale(15),
+        paddingRight: scale(30),
+      }}>
+      <View style={{ flexDirection: "row", marginBottom: verticalScale(8) }}>
+        <Text
+          style={{
+            fontFamily: "Pretendard",
+            fontStyle: "normal",
+            fontWeight: "700",
+            fontSize: 15,
+            textAlign: "left",
+            color: "#FFFFFF",
+            marginRight: scale(5),
+          }}>
+          이안
+        </Text>
+        <Text
+          style={{
+            fontFamily: "Pretendard",
+            fontStyle: "normal",
+            fontWeight: "600",
+            fontSize: 15,
+            textAlign: "left",
+            color: "#737373",
+          }}>
+          헤어샵 이름
+        </Text>
+      </View>
       <Text
         style={{
           fontFamily: "Pretendard",
@@ -79,8 +119,28 @@ const DesignerItem = () => (
           fontSize: 12,
           textAlign: "left",
           color: "rgba(255, 255, 255, 0.7)",
+          marginBottom: verticalScale(8),
         }}>
         울산 남구 수암로 148 홈플러스 울산남구점 옥상층(5층)
+      </Text>
+      <View style={{ width: "100%", flexDirection: "row", flexWrap: "wrap" }}>
+        <TagItem value="다운펌" />
+        <TagItem value="남성컷" />
+        <TagItem value="커트" />
+      </View>
+    </View>
+    <View style={{ width: "5%", paddingTop: verticalScale(3) }}>
+      <StarIcon fill="#ffce00" width={17} height={17} />
+      <Text
+        style={{
+          fontFamily: "Pretendard",
+          fontStyle: "normal",
+          fontWeight: "500",
+          fontSize: 10,
+          textAlign: "left",
+          color: "rgba(255, 255, 255, 0.7);",
+        }}>
+        4.7
       </Text>
     </View>
   </View>
@@ -135,7 +195,7 @@ export default function Loading() {
             paddingBottom: verticalScale(13),
             paddingLeft: scale(18),
           }}>
-          <SearchIcon />
+          <SearchIcon style={{ marginRight: scale(10) }} />
           <TextInput
             placeholder="키워드를 검색해 주세요"
             placeholderTextColor="#C8C8C8"
