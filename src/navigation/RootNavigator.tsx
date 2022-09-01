9e896fdc0dc002e6a9616ff42046376143bac21a
kkm <chachami66@gmail.com>
2022-09-01 15:59:01 +0900

    impr. user page look up design complete

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -6,6 +6,7 @@ import VirtualStyling from "../screens/VirtualStyling";
 import ProfileSelection from "../screens/ProfileSelection";
 import DesignerProfile from "../screens/DesignerProfile";
 import DesignerList from "../screens/DesignerList";
+import UserProfileLookup from "../screens/UserProfileLookup";
 
 import Loading from "../screens/Loading";
 
@@ -15,13 +16,18 @@ export const RootNavigator = () => {
   return (
     <mainStack.Navigator
       screenOptions={{ headerShown: false }}
-      initialRouteName="Loading">
+      initialRouteName="UserProfileLookup">
       <mainStack.Screen name="Loading" component={Loading} />
       <mainStack.Screen name="Main" component={Main} />
       <mainStack.Screen name="VirtualStyling" component={VirtualStyling} />
       <mainStack.Screen name="DesignerProfile" component={DesignerProfile} />
       <mainStack.Screen name="ProfileSelection" component={ProfileSelection} />
       <mainStack.Screen name="DesignerList" component={DesignerList} />
+
+      <mainStack.Screen
+        name="UserProfileLookup"
+        component={UserProfileLookup}
+      />
     </mainStack.Navigator>
   );
 };
