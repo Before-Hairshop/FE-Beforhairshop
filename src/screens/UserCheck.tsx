d4f689b1fac385d9be292fa71a9d57ee0b14f8b2
UNayoung <skdud@ajou.ac.kr>
2022-09-05 16:48:10 +0900

    fix. user check page stylesheet

--- src/screens/UserCheck.tsx
+++ src/screens/UserCheck.tsx
@@ -9,97 +9,31 @@ const { width, height } = Dimensions.get("window");
 
 export default function UserCheck() {
   return (
-    <View
-      style={{
-        width: width,
-        height: height,
-        backgroundColor: "#191919",
-        alignItems: "center",
-      }}>
-      <Text
-        style={{
-          fontFamily: "Pretendard",
-          fontSize: 30,
-          fontWeight: "normal",
-          fontStyle: "normal",
-          lineHeight: 32,
-          letterSpacing: 0,
-          textAlign: "left",
-          color: "#ffffff",
-          marginTop: verticalScale(130),
-        }}>
-        반갑습니다!
-      </Text>
-      <Text
-        style={{
-          fontFamily: "Pretendard",
-          fontSize: 16,
-          fontWeight: "500",
-          fontStyle: "normal",
-          letterSpacing: 0,
-          textAlign: "left",
-          color: "#fc2a5b",
-          marginTop: verticalScale(15),
-          marginBottom: verticalScale(50),
-        }}>
-        헤어 디자이너인가요?
-      </Text>
+    <View style={styles.frame}>
+      <Text style={styles.welcome_text}>반갑습니다!</Text>
+      <Text style={styles.question_text}>헤어 디자이너인가요?</Text>
       <TouchableOpacity
         style={[styles.button_container, { borderColor: "#fc2a5b" }]}>
-        <View
-          style={{
-            height: "100%",
-            justifyContent: "center",
-          }}>
-          <View
-            style={{
-              width: 25,
-              height: 25,
-              borderRadius: 25 / 2,
-              backgroundColor: "#fc2a5b",
-              alignItems: "center",
-              justifyContent: "center",
-            }}>
+        <View style={styles.check_icon_container}>
+          <View style={[styles.circle, { backgroundColor: "#fc2a5b" }]}>
             <CheckIcon fill="#ffffff" />
           </View>
         </View>
-        <View
-          style={{
-            height: "100%",
-            justifyContent: "center",
-            marginLeft: scale(10),
-          }}>
-          <Text style={[styles.button_text, { opacity: 1 }]}>
+        <View style={styles.answer_container}>
+          <Text style={[styles.answer_text, { opacity: 1 }]}>
             네, 헤어 디자이너입니다.
           </Text>
         </View>
       </TouchableOpacity>
       <TouchableOpacity
         style={[styles.button_container, { borderColor: "#2f2f2f" }]}>
-        <View
-          style={{
-            height: "100%",
-            justifyContent: "center",
-          }}>
-          <View
-            style={{
-              width: 25,
-              height: 25,
-              borderRadius: 25 / 2,
-              backgroundColor: "#2f2f2f",
-              alignItems: "center",
-              justifyContent: "center",
-            }}>
+        <View style={styles.check_icon_container}>
+          <View style={styles.circle}>
             <CheckIcon fill="#5f5f5f" />
           </View>
         </View>
-        <View
-          style={{
-            height: "100%",
-            justifyContent: "center",
-            marginLeft: scale(10),
-          }}>
-          <Text style={styles.button_text}>아니요</Text>
+        <View style={styles.answer_container}>
+          <Text style={styles.answer_text}>아니요</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -107,6 +41,34 @@ export default function UserCheck() {
 }
 
 const styles = StyleSheet.create({
+  frame: {
+    width: width,
+    height: height,
+    backgroundColor: "#191919",
+    alignItems: "center",
+  },
+  welcome_text: {
+    fontFamily: "Pretendard",
+    fontSize: 30,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    lineHeight: 32,
+    letterSpacing: 0,
+    textAlign: "left",
+    color: "#ffffff",
+    marginTop: verticalScale(130),
+  },
+  question_text: {
+    fontFamily: "Pretendard",
+    fontSize: 16,
+    fontWeight: "500",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textAlign: "left",
+    color: "#fc2a5b",
+    marginTop: verticalScale(15),
+    marginBottom: verticalScale(50),
+  },
   button_container: {
     width: "89%",
     height: verticalScale(70),
@@ -126,7 +88,24 @@ const styles = StyleSheet.create({
     marginBottom: verticalScale(10),
     flexDirection: "row",
   },
-  button_text: {
+  check_icon_container: {
+    height: "100%",
+    justifyContent: "center",
+  },
+  circle: {
+    width: 25,
+    height: 25,
+    borderRadius: 25 / 2,
+    backgroundColor: "#2f2f2f",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  answer_container: {
+    height: "100%",
+    justifyContent: "center",
+    marginLeft: scale(10),
+  },
+  answer_text: {
     opacity: 0.34,
     fontFamily: "Pretendard",
     fontSize: 16,
