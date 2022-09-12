c248b9729c598c0ed98befc07249263d6e286a16
UNayoung <skdud@ajou.ac.kr>
2022-09-12 18:18:51 +0900

    impl. get cookies from webview

--- src/screens/Loading.tsx
+++ src/screens/Loading.tsx
@@ -6,11 +6,44 @@ import {
   Animated,
   Alert,
   TouchableOpacity,
+  Linking,
+  Modal,
+  SafeAreaView,
+  Platform,
+  NativeSyntheticEvent,
 } from "react-native";
-import React, { useEffect, useLayoutEffect, useRef } from "react";
+import React, { createRef, useEffect, useLayoutEffect, useRef } from "react";
 import { scale, verticalScale } from "../utils/scale";
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import { WebView } from "react-native-webview";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  WebViewMessage,
+  WebViewNavigation,
+} from "react-native-webview/lib/WebViewTypes";
+import CookieManager from "@react-native-cookies/cookies";
+
+const GOOGLE_SOCIAL_LOGIN_URI =
+  "http://localhost:8080/oauth2/authorization/google";
+// "http://localhost:8080/logout";
+const NAVER_SOCIAL_LOGIN_URI =
+  "http://localhost:8080/oauth2/authorization/naver";
+// "http://localhost:8080/logout";
+const KAKAO_SOCIAL_LOGIN_URI =
+  "http://127.0.0.1:8080/oauth2/authorization/kakao";
+
+const userAgent =
+  // "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
+  // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+  // "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
+  Platform.OS === "android"
+    ? "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
+    : "AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75";
+// "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
+// "customUserAgent";
+// "userAgent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
 
 const Logo = require("../assets/images/Logo.png");
 
@@ -28,7 +61,25 @@ const wait = timeToDelay =>
 
 export default function Loading() {
   const navigation = useNavigation();
+
   const [isUserLoggedIn, setIsUserLoggedIn] = useState("");
+  const [socialModalVisible, setSocialModalVisible] = useState(false);
+
+  const signInWithGoogle = () => {
+    setSocialModalVisible(true);
+    // const result = await axios.get(
+    //   "http://localhost:8080/oauth2/authorization/google",
+    // );
+    // Linking.openURL("http://localhost:8080/oauth2/authorization/google");
+  };
+
+  const signInWithKakao = () => {
+    setSocialModalVisible(true);
+    // const result = await axios.get(
+    //   "http://localhost:8080/oauth2/authorization/google",
+    // );
+    // Linking.openURL("http://localhost:8080/oauth2/authorization/google");
+  };
 
   useEffect(() => {
     //로그인 여부 확인
@@ -51,6 +102,52 @@ export default function Loading() {
   }, [isUserLoggedIn]);
 
   let opacity = new Animated.Value(0);
+
+  // useEffect(() => {
+  //   let getCookies = () =>
+  //     CookieManager.getAll(useWebKit).then(cookies => {
+  //       console.log("CookieManager.get =>", cookies);
+  //     });
+  //   if (Platform.OS === "android") {
+  //     getCookies = () =>
+  //       CookieManager.get("https://linkedin.com").then(cookies => {
+  //         console.log("CookieManager.get =>", cookies);
+  //       });
+  //   }
+  //   getCookies();
+  // }, []);
+
+  let webViewRef = createRef<WebView>();
+
+  // const CHECK_COOKIE: string = ReactNativeWebView.postMessage(
+  //   "Cookie: " + document.cookie,
+  // );
+  // true;
+  const onNavigationStateChange = async (
+    navigationState: WebViewNavigation,
+  ) => {
+    // console.log(navigationState);
+    // CookieManager.getAll(true).then(res => {
+    //   // console.log(res.ACCOUNT_CHOOSER);
+    //   console.log(res);
+    // });
+    const cookies = await CookieManager.get("http://localhost:8080");
+    console.log(cookies.JSESSIONID.value);
+    // if (webViewRef.current) {
+    //   console.log("onNavigationStateChange");
+    //   // webViewRef.current.injectJavaScript("Data from React Native App");
+
+    // }
+  };
+
+  // const onMessage = (event: NativeSyntheticEvent<WebViewMessage>) => {
+  //   const { data } = event.nativeEvent;
+  //   console.log("onMessage");
+
+  //   if (data.includes("Cookie:")) {
+  //     //
+  //   }
+  // };
 
   return (
     <View
@@ -104,13 +201,13 @@ export default function Loading() {
             SNS 계정으로 간편가입하기{" "}
           </Text>
           <View style={{ flexDirection: "row", margin: verticalScale(23) }}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={signInWithGoogle}>
               <Image
                 source={require("../assets/icons/google_icon.png")}
                 style={styles.iconStyle}></Image>
             </TouchableOpacity>
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={signInWithKakao}>
               <Image
                 source={require("../assets/icons/kakao_icon.png")}
                 style={styles.iconStyle}></Image>
@@ -118,6 +215,34 @@ export default function Loading() {
           </View>
         </Animated.View>
       </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={socialModalVisible}>
+        <WebView
+          ref={webViewRef}
+          cacheEnabled={false}
+          // onLoad={() => {
+          //   console.log(
+          //     webViewRef.postMessage("전송 데이터(React) : 웹으로 데이터 전송"),
+          //   );
+          // }}
+          // originWhitelist={["*"]}
+          source={{ uri: NAVER_SOCIAL_LOGIN_URI }}
+          userAgent={userAgent}
+          // WebView 로딩이 시작되거나 끝나면 호출해주는 것
+          onNavigationStateChange={onNavigationStateChange}
+          // onMessage={onMessage}
+          onMessage={evt => {
+            console.log("받은 데이터: " + evt);
+          }}
+          // userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+          sharedCookiesEnabled={true}
+          thirdPartyCookiesEnabled={true}
+          // useWebKit={true}
+          javaScriptEnabled={true}
+        />
+      </Modal>
     </View>
   );
 }
